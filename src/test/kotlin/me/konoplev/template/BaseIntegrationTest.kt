3eca629ab16c1720cf771461db9163f6a739aac5
Alexander Konoplev <alexander.konoplev@signavio.com>
2023-05-13 11:39:48 +0200

    fix failed tests

--- src/test/kotlin/me/konoplev/template/BaseIntegrationTest.kt
+++ src/test/kotlin/me/konoplev/template/BaseIntegrationTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.TestInstance
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.*
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -13,6 +14,7 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestPropertySource(properties = ["spring.jpa.hibernate.ddl-auto=validate"])
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DirtiesContext
 abstract class BaseIntegrationTest {
     companion object {
         private val container: PostgreSQLContainer<Nothing> = PostgreSQLContainer<Nothing>("postgres:14.1").apply {
@@ -24,10 +26,6 @@ abstract class BaseIntegrationTest {
         @JvmStatic
         @DynamicPropertySource
         fun properties(registry: DynamicPropertyRegistry) {
-            initialize(registry)
-        }
-
-        private fun initialize(registry: DynamicPropertyRegistry) {
             container.start()
             registry.add("spring.datasource.url", container::getJdbcUrl)
             registry.add("spring.datasource.username", container::getUsername)
@@ -37,15 +35,6 @@ abstract class BaseIntegrationTest {
         @AfterAll
         fun teardown() {
             container.stop()
-        }
-
-        @TestExecutionListeners(InitializableTestExecutionListener::class)
-        abstract class InitializableTestExecutionListener : TestExecutionListener {
-            override fun beforeTestMethod(testContext: TestContext) {
-                if (!container.isRunning) {
-                    initialize(testContext.applicationContext.getBean(DynamicPropertyRegistry::class.java))
-                }
-            }
         }
     }
 
